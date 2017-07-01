73f50207504aad78b91f1f7588aaa9b24c4f45e0
arkenthera <arkenthera@gmail.com>
2017-07-01 19:15:28 +0300

    Add emoji count

--- migrate_to_elasticsearch.py
+++ migrate_to_elasticsearch.py
@@ -1,3 +1,3 @@
 from to_es import *
 
-put_words_to_es()
+put_emojis_to_es()
--- to_es.py
+++ to_es.py
@@ -2,32 +2,35 @@ from database import *
 import datetime
 import requests
 import json
+import re
 
 #KAIROS_SERVER = "http://localhost:8000"
 ES_SERVER_MESSAGECOUNT = "http://40.121.158.220:9200/discord_reindex/discord_message_count/"
 ES_SERVER_COMMANDS = "http://40.121.158.220:9200/discord_command/discord_command_count/"
+ES_SERVER_EMOJI = "http://40.121.158.220:9200/discord_emoji/discord_emoji_count/"
 ES_SERVER_WORDS = "http://40.121.158.220:9200/discord_words/word_type/"
 
-def add_emoji(message, emoji):
+def add_emoji(message, emoji, index_id):
     timestamp = (int((message.date - datetime.datetime(1970, 1, 1)).total_seconds()))
 
     json_str = {"timestamp": timestamp,
                 "message_id": message.message_id,
                 "emoji": emoji,
-                "command_count":1,
+                "emoji_count":1,
                 "channel": message.channel.name,
                 "author": message.user.name
                 }
     headers = {'Content-Type': 'application/json'}
     data = json.dumps(json_str)
     print(data)
-    # try:
-    #     response = requests.put("{}{}".format(ES_SERVER_COMMANDS, message.message_id), data, headers=headers)
-    #     if response.status_code == 201 or response.status_code == 200:
-    #         print("ES Successful {}".format(response.status_code))
-    # except Exception as error:
-    #     print("Error trying to send data to ES")
-    #     print(error)
+    print(index_id)
+    try:
+        response = requests.put("{}{}".format(ES_SERVER_EMOJI, index_id), data, headers=headers)
+        if response.status_code == 201 or response.status_code == 200:
+            print("Emoji - ES Successful {}".format(response.status_code))
+    except Exception as error:
+        print("Error trying to send data to ES")
+        print(error)
 
 def add_command(message,command):
     timestamp = (int((message.date - datetime.datetime(1970, 1, 1)).total_seconds()))
@@ -44,7 +47,7 @@ def add_command(message,command):
     try:
         response = requests.put("{}{}".format(ES_SERVER_COMMANDS, message.message_id), data, headers=headers)
         if response.status_code == 201 or response.status_code == 200:
-            print("ES Successful {}".format(response.status_code))
+            print("Command - ES Successful {}".format(response.status_code))
     except Exception as error:
         print("Error trying to send data to ES")
         print(error)
@@ -52,8 +55,8 @@ def add_command(message,command):
 def add_message_count(message):
     timestamp = (int((message.date - datetime.datetime(1970, 1, 1)).total_seconds()))
 
+    # Check commands
     prefix = ["!", "~"]
-
     message_content = message.content
     for p in prefix:
         if not message_content.startswith(p):
@@ -63,6 +66,27 @@ def add_message_count(message):
 
         add_command(message, command)
 
+    # Check emojis
+    emojis = ["FeelsMetalMan", "goldMetal", "petaLUL", "PogChamp", "puke", "Eargasm", "EleGiggle", "FeelsBadMan",
+              "FeelsRageMan", "FeelsBlackMetal", "FeelsCoreMan",
+              "FeelsLongHaired", "FeelsCorpsePaint", "FeelsBleagh", "FeelsProgMan", "FeelsMetalHead", "thinkmetal",
+              "fedora", "trump1", "nm", "suicide", "slamslug", "arkhammer", "uberthink", "nerdal", "rip",
+              "thangery", "thunking", "grindgoat", "CoreWeekly", "CoreYearly", "pukethink1", "FeelsSubLevel",
+              "genrepolice", "FeelsAmazingMan", "FailFish", "WutFace", "FeelsWeebMan", "FeelsTootMan", "CoreEternity",
+              "FeelsNargaMan", "hmm", "glamslug", "FeelsCossackMan", "boing", "cutyou", "FeelsToneMan"]
+    for emoji in emojis:
+        str = "<:{}".format(emoji)
+
+        matches = [m.start() for m in re.finditer(str, message_content)]
+        if len(matches) > 0:
+            i = 0
+            for match in matches:
+                index = message.message_id
+                index = "{}_{}".format(index, i)
+                add_emoji(message, emoji, index)
+                i = i + 1
+
+    # message count stuff
     json_str = { "timestamp": timestamp,
                  "message_id": message.message_id,
                  "message_count": 1.0,
@@ -135,16 +159,23 @@ def put_commands_to_es():
 
             add_command(message, command)
 
-# def put_emojis_to_es():
-#     messages = get_all_messages()
-#
-#     emojis = ["FeelsMetalMan", "goldMetal", "petaLUL", "PogChamp", "puke", "Eargasm", "EleGiggle", "FeelsBadMan", "FeelsRageMan", "FeelsBlackMetal", "FeelsCoreMan",
-#               "FeelsLongHaired","FeelsCorpsePaint", "FeelsBleagh", "FeelsProgMan", "FeelsMetalHead", "thinkmetal", "fedora", "trump1", "nm", "suicide", "slamslug", "arkhammer", "uberthink", "nerdal", "rip",
-#               "thangery", "thunking", "grindgoat", "CoreWeekly", "CoreYearly", "pukethink1", "FeelsSubLevel", "genrepolice", "FeelsAmazingMan", "FailFish", "WutFace", "FeelsWeebMan", "FeelsTootMan", "CoreEternity", "FeelsNargaMan", "hmm", "glamslug", "FeelsCossackMan", "boing", "cutyou", "FeelsToneMan"]
-#     for message in messages:
-#         message_content = message.content
-#
-#         for emoji in emojis:
-#             str = "<:{}".format(emoji)
-#
-#             if str in
+def put_emojis_to_es():
+    messages = get_all_messages()
+
+    emojis = ["FeelsMetalMan", "goldMetal", "petaLUL", "PogChamp", "puke", "Eargasm", "EleGiggle", "FeelsBadMan", "FeelsRageMan", "FeelsBlackMetal", "FeelsCoreMan",
+              "FeelsLongHaired","FeelsCorpsePaint", "FeelsBleagh", "FeelsProgMan", "FeelsMetalHead", "thinkmetal", "fedora", "trump1", "nm", "suicide", "slamslug", "arkhammer", "uberthink", "nerdal", "rip",
+              "thangery", "thunking", "grindgoat", "CoreWeekly", "CoreYearly", "pukethink1", "FeelsSubLevel", "genrepolice", "FeelsAmazingMan", "FailFish", "WutFace", "FeelsWeebMan", "FeelsTootMan", "CoreEternity", "FeelsNargaMan", "hmm", "glamslug", "FeelsCossackMan", "boing", "cutyou", "FeelsToneMan"]
+    for message in messages:
+        message_content = message.content
+
+        for emoji in emojis:
+            str = "<:{}".format(emoji)
+
+            matches = [m.start() for m in re.finditer(str, message_content)]
+            if len(matches) > 0:
+                i = 0
+                for match in matches:
+                    index = message.message_id
+                    index = "{}_{}".format(index,i)
+                    add_emoji(message, emoji, index)
+                    i = i + 1
