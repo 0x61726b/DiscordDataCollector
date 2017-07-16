e95cea9c3199786d661e92461c299946b3d79b0b
arkenthera <arkenthera@gmail.com>
2017-07-16 20:45:52 +0300

    AAT

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-# DB_FILE = "satoshi"
-# ES_SERVER = "http://40.121.158.220:9200/"
-# ES_INDEX_MESSAGECOUNT = "aat_message/aat_message_type/"
-# ES_INDEX_COMMANDS = "aat_command/aat_command_type/"
-# ES_INDEX_EMOJI = ""
-# ES_INDEX_WORDS = "aat_words/aat_words_type/"
-# CHECK_EMOJI = False
-
-DB_FILE = "metal_music_discord"
+DB_FILE = "satoshi"
 ES_SERVER = "http://40.121.158.220:9200/"
-ES_INDEX_MESSAGECOUNT = "discord_reindex/discord_command_count/"
-ES_INDEX_COMMANDS = "discord_command/discord_emoji_count/"
-ES_INDEX_EMOJI = "discord_emoji/discord_emoji_count/"
-ES_INDEX_WORDS = "discord_words/word_type/"
-CHECK_EMOJI = True
+ES_INDEX_MESSAGECOUNT = "aat_message/aat_message_type/"
+ES_INDEX_COMMANDS = "aat_command/aat_command_type/"
+ES_INDEX_EMOJI = ""
+ES_INDEX_WORDS = "aat_words/aat_words_type/"
+CHECK_EMOJI = False
+
+# DB_FILE = "metal_music_discord"
+# ES_SERVER = "http://40.121.158.220:9200/"
+# ES_INDEX_MESSAGECOUNT = "discord_reindex/discord_command_count/"
+# ES_INDEX_COMMANDS = "discord_command/discord_emoji_count/"
+# ES_INDEX_EMOJI = "discord_emoji/discord_emoji_count/"
+# ES_INDEX_WORDS = "discord_words/word_type/"
+# CHECK_EMOJI = True
--- get_message_history.py
+++ get_message_history.py
@@ -124,7 +124,7 @@ async def data_future(i_channel_name, before):
         if channel.name == i_channel_name:
             print("Channel: {}".format(channel.name))
             try:
-                async for message in client.logs_from(channel, limit=fetch_limit, after=before):
+                async for message in client.logs_from(channel, limit=fetch_limit, before=before):
                     count = count + 1
                     message_list.append(message)
                     if count % 1000:
@@ -157,19 +157,15 @@ async def data_future(i_channel_name, before):
 @client.event
 async def on_ready():
     logger.info("Bot is ready.")
-    before_date = datetime.datetime(2017,6,30)
-    channels = ["info", "announcements", "general", "black",
-                "death", "core", "heavy", "doom", "alt", "grind", "prog",
-                "melodic", "thrash", "brutal", "weekly-discussion", "music-bot",
-                "cytube", "merch", "oc", "non-metal", "off-topic", "gaming", "weeb", "shitposting", "mod", "suggestion-box"]
+    before_date = datetime.datetime(2017,7,18)
 
 
-    tasks =  [data_future("weekly-discussion",before_date)]
+    tasks =  [data_future("general-chat",before_date)]
     asyncio.ensure_future(asyncio.gather(*tasks))
 
 
 
-with open("token_stats", "r") as tokenfile:
+with open("token", "r") as tokenfile:
     token = ""
     for line in tokenfile:
         token += line
