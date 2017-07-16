52ad08524f36fcb8996844ffd38995248204faa0
arkenthera <arkenthera@gmail.com>
2017-07-16 18:32:05 +0300

    Fix index name

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DB_FILE = "metal_music_discord"
 ES_SERVER = "http://40.121.158.220:9200/"
-ES_INDEX_MESSAGECOUNT = "discord_reindex/discord_command_count/"
-ES_INDEX_COMMANDS = "discord_command/discord_emoji_count/"
+ES_INDEX_MESSAGECOUNT = "discord_reindex/discord_message_count/"
+ES_INDEX_COMMANDS = "discord_command/discord_command_count/"
 ES_INDEX_EMOJI = "discord_emoji/discord_emoji_count/"
 ES_INDEX_WORDS = "discord_words/word_type/"
 CHECK_EMOJI = True
