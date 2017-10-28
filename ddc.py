ed189c8f1a1a23b196796297145fbe7aaffeaaa2
arkenthera <arkenthera@gmail.com>
2017-10-28 22:11:54 +0300

    Stuff

--- ddc.py
+++ ddc.py
@@ -76,7 +76,7 @@ class DDC(discord.Client):
 
     async def cmd_aq(self, message, channel):
         try:
-            split = message.content.split('{}{} '.format(PREFIX, 'q'))
+            split = message.content.split('{}{} '.format(PREFIX, 'aq'))
             user_name = split[1]
 
             clients = self.get_all_members()
