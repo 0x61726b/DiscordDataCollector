53c4401122643afaa941e9aecd9d780fe06e876d
arkenthera <arkenthera@gmail.com>
2017-10-28 19:25:40 +0300

    Added stuff

--- ddc.py
+++ ddc.py
@@ -4,6 +4,8 @@ import logging
 from database import *
 from to_es import *
 import os
+import sys
+import inspect
 from random import randint
 
 logging.basicConfig(level=logging.INFO)
@@ -13,159 +15,341 @@ handler = logging.FileHandler(filename='satoshi.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+class Response:
+    def __init__(self, content, reply=False, delete_after=0):
+        self.content = content
+        self.reply = reply
+        self.delete_after = delete_after
 
-class Presence():
-    def __init__(self):
-        self.canSetPresence = True
+PREFIX = "!"
 
-    def presence(self):
-        return self.canSetPresence
+class MessageState:
+    def __init__(self, time, user):
+        self.time = time
+        self.user = user
 
-    def setPresence(self,v):
-        self.canSetPresence = v
+class DDC(discord.Client):
+    def __init__(self,token):
+        self.auth = token
+        super().__init__()
 
-p = Presence()
-client = discord.Client()
+        self.queue = asyncio.Queue()
 
-async def add_user_to_db(member):
-    user_info = await client.get_user_info(member.id)
-    avatar_url = user_info.avatar_url
-    isBot = user_info.bot
-    created_at = user_info.created_at
-    discriminator = user_info.discriminator
-    display_name = user_info.display_name
-    name = user_info.name
-    join_date = member.joined_at
+    async def get_random_message(self, user_name):
+        try:
+            target_user = User.select().where(User.name == user_name).get()
+            message_array = []
+            messages = Message.select().order_by(Message.date).where((Message.channel != EXCLUDE_CHANNEL_ID),(Message.user == target_user.discord_id))
+            for message in messages:
+                message_array.append(message)
 
-    logger.info("Adding new user to DB {} {} {} {} {} {} {} {}".format(member.id,name,display_name,join_date,created_at,isBot,avatar_url,discriminator))
+            unqualified = True
+            random_msg = None
 
-    try:
-        return User.create(discord_id=int(member.id),avatarUrl=avatar_url,isBot=isBot,registerDate=created_at,discriminator=discriminator,display_name=display_name,name=name,joinDate=join_date)
-    except Exception as error:
-        logger.debug("User.create failed. Probably due to primary key already existing. {}".format(error))
-        return None
+            len_messages = len(message_array)
+            max_iterations = 10
+            i = 0
+            while unqualified:
+                random_nmb = randint(0, len_messages - 1)
+                random_msg = message_array[random_nmb]
+                if random_msg:
+                    if "<:" in random_msg.content or len(random_msg.content) < 50 or "<@" in random_msg.content or "://" in random_msg.content:
+                        unqualified = True
+                    else:
+                        unqualified = False
+                i = i + 1
+
+                if i > max_iterations:
+                    unqualified = False
+                    random_msg = None
+            if random_msg:
+                content = random_msg.content
+                user_id = random_msg.user.discord_id
+                user_info = await self.get_user_info(user_id)
+                if user_info:
+                    content = "```xml\n[{}/{}] <{}> {}```".format(random_nmb, len_messages, user_info.display_name,content)
+            else:
+                content = ""
+            return content
+        except:
+            return ""
+
+    async def cmd_q(self, channel, user_name):
+        try:
+            random_msg = await self.get_random_message(user_name)
+            if len(random_msg) > 0:
+                await self.send_message(channel, random_msg)
+            else:
+                await self.send_message(channel, EMOJI_ON_FAIL)
+        except:
+            await self.send_message(channel, EMOJI_ON_FAIL)
+
+    async def random_msg_task(self):
+        await self.wait_until_ready()
+        channel = self.get_channel(RANDOM_QUOTE_TARGET_CHANNEL_ID)
+
+        last_message_time = datetime.datetime.now()
+        last_message_user = None
+        should_post = False
+
+        interval = 2
+
+        print("Starting random msg task")
+
+        while not self.is_closed:
+            qsize = self.queue.qsize()
+
+            if qsize != 0:
+                message_state = await self.queue.get()
+                if message_state:
+                    last_message_time = message_state.time
+                    last_message_user = message_state.user
+
+            now = datetime.datetime.now()
+            diff = now - last_message_time
+
+            if diff.total_seconds() > interval:
+                should_post = True
+
+            if last_message_user is not None and last_message_user.id == BOT_CLIENT_ID:
+                should_post = False
+
+            if should_post:
+                users = User.select()
+                users_array = []
+                interval = RANDOM_QUOTE_INTERVAL
+
+                for user in users:
+                    users_array.append(user)
+                users_len = len(users_array)
+                random_user_index = randint(0, users_len - 1)
+                random_user = users_array[random_user_index]
+                if random_user and random_user.discord_id != BOT_CLIENT_ID:
+                    try:
+                        random_msg = await self.get_random_message(random_user.name)
+                        if len(random_msg) > 0:
+                            await self.send_message(channel, random_msg)
+                            await asyncio.sleep(5)
+                        else:
+                            await self.send_message(channel, EMOJI_ON_FAIL)
+                    except:
+                        await self.send_message(channel, EMOJI_ON_FAIL)
+                else:
+                    await self.send_message(channel, EMOJI_ON_FAIL)
+            else:
+                await asyncio.sleep(5)
 
 
-def add_channel_to_db(channel):
-    channel_name = channel.name
-    channel_id = channel.id
-    topic = "" if channel.topic == None else channel.topic
-    is_voice = False
-    try:
-        if channel.type == "voice":
-            is_voice = True
-    except:
+
+
+    async def on_ready(self):
+        logger.info("Bot is ready.")
+
+        try:
+            await self.change_presence(game=discord.Game(name='Clannad sucks'))
+        except:
+            logging.warning("Could not set presence")
+    async def on_member_join(self,member):
+        await self.add_user_to_db(member)
+    async def add_user_to_db(self,member):
+        user_info = await self.get_user_info(member.id)
+        avatar_url = user_info.avatar_url
+        isBot = user_info.bot
+        created_at = user_info.created_at
+        discriminator = user_info.discriminator
+        display_name = user_info.display_name
+        name = user_info.name
+        join_date = member.joined_at
+
+        logger.info("Adding new user to DB {} {} {} {} {} {} {} {}".format(member.id,name,display_name,join_date,created_at,isBot,avatar_url,discriminator))
+
+        try:
+            return User.create(discord_id=int(member.id),avatarUrl=avatar_url,isBot=isBot,registerDate=created_at,discriminator=discriminator,display_name=display_name,name=name,joinDate=join_date)
+        except Exception as error:
+            logger.debug("User.create failed. Probably due to primary key already existing. {}".format(error))
+            return None
+    async def add_channel_to_db(self,channel):
+        channel_name = channel.name
+        channel_id = channel.id
+        topic = "" if channel.topic == None else channel.topic
         is_voice = False
+        try:
+            if channel.type == "voice":
+                is_voice = True
+        except:
+            is_voice = False
 
 
-    logger.info("Adding new channel to DB {} {}".format(channel_id,channel_name))
+        logger.info("Adding new channel to DB {} {}".format(channel_id,channel_name))
 
-    try:
-        return Channel.create(channel_id=int(channel_id),name=channel_name,topic=topic,is_voice=is_voice)
-    except Exception as error:
-        logger.info("Channel.create failed. {}".format(error))
-        return None
+        try:
+            return Channel.create(channel_id=int(channel_id),name=channel_name,topic=topic,is_voice=is_voice)
+        except Exception as error:
+            logger.info("Channel.create failed. {}".format(error))
+            return None
+    async def add_message_to_db(self,message):
+        content = message.content
+        message_id = int(message.id)
+        user_id = int(message.author.id)
+        channel_id = int(message.channel.id)
+        date = message.timestamp
+        has_mentions = False
+        is_pinned = message.pinned
 
-async def add_message_to_db(message):
-    content = message.content
-    message_id = int(message.id)
-    user_id = int(message.author.id)
-    channel_id = int(message.channel.id)
-    date = message.timestamp
-    has_mentions = False
-    is_pinned = message.pinned
+        try:
+            user = User.select().where(User.discord_id==user_id).get()
+        except:
+            logger.info("Could not find user. Adding")
+            user = await self.add_user_to_db(message.author)
+            if user is None:
+                return
+        try:
+            channel = Channel.select().where(Channel.channel_id==channel_id).get()
+        except:
+            logger.info("Could not find channel. Adding")
+            channel = self.add_channel_to_db(message.channel)
+            if channel is None:
+                logger.error("Channel error")
+                return
 
-    try:
-        user = User.select().where(User.discord_id==user_id).get()
-    except:
-        logger.info("Could not find user. Adding")
-        user = await add_user_to_db(message.author)
-        if user is None:
+
+        if len(message.mentions) != 0:
+            has_mentions = True
+
+        logger.info("New message: {} {} {} {} {}".format(user_id, user.name, user.display_name, channel.name, content))
+
+        try:
+            message = Message.create(message_id=message_id,content=content,user=user, channel=channel, date=date,has_mentions=has_mentions,is_pinned=is_pinned)
+            add_message_count(message)
+        except Exception as error:
+            logger.debug("Message.create failed. Probably due to primary key already existing. {}".format(error))
+    async def on_message(self, message):
+        message_content = message.content.strip()
+
+        await self.add_message_to_db(message)
+        await self.queue.put(MessageState(datetime.datetime.now(),message.author))
+        qsize = self.queue.qsize()
+
+        if not message_content.startswith(PREFIX):
             return
-    try:
-        channel = Channel.select().where(Channel.channel_id==channel_id).get()
-    except:
-        logger.info("Could not find channel. Adding")
-        channel = add_channel_to_db(message.channel)
-        if channel is None:
-            logger.error("Channel error")
+
+        command, *args = message_content.split()
+        command = command[len(PREFIX):].lower().strip()
+
+        handler = getattr(self, 'cmd_%s' % command, None)
+        if not handler:
             return
 
+        argspec = inspect.signature(handler)
+        params = argspec.parameters.copy()
 
-    if len(message.mentions) != 0:
-        has_mentions = True
+        try:
+            handler_kwargs = {}
+            if params.pop('message', None):
+                handler_kwargs['message'] = message
 
-    logger.info("New message: {} {} {} {} {}".format(user_id, user.name, user.display_name, channel.name, content))
+            if params.pop('channel', None):
+                handler_kwargs['channel'] = message.channel
 
+            if params.pop('author', None):
+                handler_kwargs['author'] = message.author
+
+            if params.pop('server', None):
+                handler_kwargs['server'] = message.server
+
+            if params.pop('user_mentions', None):
+                handler_kwargs['user_mentions'] = list(map(message.server.get_member, message.raw_mentions))
+
+            if params.pop('channel_mentions', None):
+                handler_kwargs['channel_mentions'] = list(map(message.server.get_channel, message.raw_channel_mentions))
+
+            args_expected = []
+            for key, param in list(params.items()):
+                doc_key = '[%s=%s]' % (key, param.default) if param.default is not inspect.Parameter.empty else key
+                args_expected.append(doc_key)
+
+                if not args and param.default is not inspect.Parameter.empty:
+                    params.pop(key)
+                    continue
+
+                if args:
+                    arg_value = args.pop(0)
+                    handler_kwargs[key] = arg_value
+                    params.pop(key)
+
+            response = await handler(**handler_kwargs)
+
+            if response and isinstance(response, Response):
+                content = response.content
+                if response.reply:
+                    content = '%s, %s' % (message.author.mention, content)
+
+                sentmsg = await self.safe_send_message(
+                    message.channel, content,
+                    expire_in=response.delete_after
+                )
+        except Exception as ex:
+            logger.error("Error handling command")
+            content = "I've run into a problem while processing your command"
+            sentmsg = await self.safe_send_message(
+                message.channel, content,
+                expire_in=10
+            )
+            print(ex)
+    async def safe_send_message(self, dest, content, *, tts=False, expire_in=0, also_delete=None, quiet=False):
+        msg = None
+        try:
+            msg = await self.send_message(dest, content, tts=tts)
+
+            if msg and expire_in:
+                asyncio.ensure_future(self._wait_delete_msg(msg, expire_in))
+
+            if also_delete and isinstance(also_delete, discord.Message):
+                asyncio.ensure_future(self._wait_delete_msg(also_delete, expire_in))
+
+        except discord.Forbidden:
+            if not quiet:
+                self.safe_print("Warning: Cannot send message to %s, no permission" % dest.name)
+
+        except discord.NotFound:
+            if not quiet:
+                self.safe_print("Warning: Cannot send message to %s, invalid channel?" % dest.name)
+
+        return msg
+    async def _wait_delete_msg(self, message, after):
+        await asyncio.sleep(after)
+        await self.safe_delete_message(message)
+    async def safe_delete_message(self, message, *, quiet=False):
+        try:
+            return await self.delete_message(message)
+
+        except discord.Forbidden:
+            if not quiet:
+                self.safe_print("Warning: Cannot delete message %s no permission" % message.clean_content)
+
+        except discord.NotFound:
+            if not quiet:
+                self.safe_print("Warning: Cannot delete message %s message not found" % message.clean_content)
+    def run(self):
+        try:
+            self.loop.create_task(self.random_msg_task())
+            self.loop.run_until_complete(self.start(self.auth))
+        except discord.errors.LoginFailure:
+            logger.error("Login error")
+
+        finally:
+            logger.info("Exiting...")
+            self.loop.close()
+
+
+if __name__ == '__main__':
     try:
-        message = Message.create(message_id=message_id,content=content,user=user, channel=channel, date=date,has_mentions=has_mentions,is_pinned=is_pinned)
-        add_message_count(message)
-    except Exception as error:
-        logger.debug("Message.create failed. Probably due to primary key already existing. {}".format(error))
-
-@client.event
-async def on_member_join(member):
-    logger.info("New member join!")
-
-    add_user_to_db(member)
-
-
-@client.event
-async def on_ready():
-    logger.info("Bot is ready.")
-
-    # If users table is empty, add users
-    users = User.select()
-    channels = Channel.select()
-
-    if len(users) == 0:
-        for member in client.get_all_members():
-            await add_user_to_db(member)
-
-    if len(channels) == 0:
-        for channel in client.get_all_channels():
-            add_channel_to_db(channel)
-
-
-
-
-@client.event
-async def on_message(message):
-    if p.presence():
-        logger.info("Setting presence...")
-        await client.change_presence(game=discord.Game(name='Building Skynet...'))
-        p.setPresence(False)
-
-    await add_message_to_db(message)
-
-
-message_array = []
-messages = Message.select().order_by(Message.date).join(Channel).where(Channel.channel_id != "349160726029533186")
-
-for message in messages:
-    message_array.append(message)
-
-async def random_message_background():
-    await client.wait_until_ready()
-    channel = client.get_channel('327792400527261696')
-    time_limit = 60 * 60 * 3
-    while not client.is_closed:
-        len_messages = len(message_array)
-        random_nmb = randint(0, len_messages - 1)
-        random_msg = message_array[random_nmb]
-        if random_msg:
-            content = random_msg.content
-            user_id = random_msg.user.discord_id
-            user_info = await client.get_user_info(user_id)
-            if user_info:
-                content = "```css\n[{}/{}] <{}> {}```".format(random_nmb, len_messages, user_info.display_name, content)
-                await client.send_message(channel, content)
-        await asyncio.sleep(time_limit)
-
-
-with open("token", "r") as tokenfile:
-    token = ""
-    for line in tokenfile:
-        token += line
-    client.loop.create_task(random_message_background())
-    client.run(token)
+        with open("token", "r") as tokenfile:
+            token = ""
+            for line in tokenfile:
+                token += line
+            bot = DDC(token)
+            bot.run()
+    except KeyboardInterrupt as ex:
+        print("Keyboard interrupted")
+        sys.exit(0)
